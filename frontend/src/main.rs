7f0ab995d6ec9fb80d8d6eca7dc4b91d0ba7d139
philipdaquin <philipdaquin20@gmail.com>
2022-03-20 20:51:27 +0800

    frontend

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+mod api;
+mod types;
+mod service;
+pub mod components;
+mod hooks;
+pub mod router;
+
+use wasm_bindgen::prelude::*;
+use crate::app::App;
+
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+pub fn main() -> Result<(), JsValue> {
+    #[cfg(debug_assertions)]
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
+    Ok(())
 }
