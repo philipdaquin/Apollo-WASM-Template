b4c80bd94966fe1e01f8e3d31b13379297069c83
philipdaquin <philipdaquin20@gmail.com>
2022-04-28 13:59:14 +0800

    removed old backend, replaced with template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,4 +1,4 @@
-use backend::server::new_server;
+use products::server::new_server;
 use failure::Error;
 
 
@@ -7,7 +7,7 @@ async fn main() -> Result<(), Error> {
     let port = std::env::var("PORT")
         .ok()
         .and_then(|port| port.parse::<u32>().ok())
-        .unwrap_or(3000);
+        .unwrap_or(4001);
         
     new_server(port)
         .await
